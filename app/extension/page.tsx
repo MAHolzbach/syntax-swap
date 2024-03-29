9a575f164d85eefa01354bc5c5e217ebcfe6ed03
MAHolzbach <maholzbach@gmail.com>
2024-03-29 10:09:07 +0100

    Lets try this again...

---
+++ app/extension/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import KeyInput from "../components/KeyInput";
+
+const Extension = () => {
+  return (
+    <div>
+      <KeyInput />
+    </div>
+  );
+};
+
+export default Extension;
